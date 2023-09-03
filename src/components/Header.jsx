81bf953bb18c56a21a7235ea248637158d3f501d
Hatif <md.hatifosmani15@gmail.com>
2023-09-04 02:45:06 +0530

    Authentication ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     const navigate = useNavigate();
     const matchRoute = (route) => {
         if(route === location.pathname){
+            console.log("here");
             return true;
         }
     };
@@ -12,13 +13,13 @@ const Header = () => {
         <div className=" bg-white shadow-sm border-b sticky top-0 z-50">
             <header className=" flex justify-between items-center px-3 mx-auto max-w-6xl">
                 <div>
-                    <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="realtor.com" className=" h-5 cursor-pointer" onClick={() => navigate()} />
+                    <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="realtor.com" className=" h-5 cursor-pointer" onClick={() => navigate("/")} />
                 </div>
                 <div>
                     <ul className=" flex space-x-10">
-                        <li className={` py-3 font-semibold text-sm text-gray-400 border-b-[3px] border-b-transparent ${matchRoute("/") && " text-black border-b-red-500"} cursor-pointer`} onClick={ () => navigate("/") }>Home</li>
-                        <li className={` py-3 font-semibold text-sm text-gray-400 border-b-[3px] border-b-transparent ${matchRoute("/offers") && " text-black border-b-red-500"} cursor-pointer`} onClick={ () => navigate("/offers") }>Offers</li>
-                        <li className={` py-3 font-semibold text-sm text-gray-400 border-b-[3px] border-b-transparent ${matchRoute("/sign-in") && " text-black border-b-red-500"} cursor-pointer`} onClick={ () => navigate("/sign-in") }>Sign In</li>
+                        <li className={` cursor-pointer py-3 font-semibold text-sm ${matchRoute('/')? " text-black border-b-[3px] border-b-red-500": " text-gray-400 border-b-[3px] border-b-transparent"}`} onClick={ () => navigate("/") }>Home</li>
+                        <li className={` cursor-pointer py-3 font-semibold text-sm ${matchRoute("/offers")? " text-black border-b-[3px] border-b-red-500": " text-gray-400 border-b-[3px] border-b-transparent"}`} onClick={ () => navigate("/offers") }>Offers</li>
+                        <li className={` cursor-pointer py-3 font-semibold text-sm ${matchRoute("/sign-in")? " text-black border-b-[3px] border-b-red-500": " text-gray-400 border-b-[3px] border-b-transparent"}`} onClick={ () => navigate("/sign-in") }>Sign In</li>
                     </ul>
                 </div>
             </header>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,63 @@
+import { useState } from "react";
+import {AiFillEye, AiFillEyeInvisible} from "react-icons/ai"
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPass, setShowPass] = useState(false)
   return (
-    <div>SignIn</div>
+    <section>
+      <h1 className=" text-3xl text-center font-bold mt-6">Sign In</h1>
+      <div className=" flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className=" md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img 
+              src="https://plus.unsplash.com/premium_photo-1661558563494-60cbd006c068?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" 
+              alt="image" 
+              className=" w-full rounded-2xl"
+            />
+        </div>
+        <div className=" w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input 
+                type="email" 
+                id="email" 
+                className=" w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                placeholder="Email Address"
+
+                />
+            <div className=" relative mb-6">
+              <input 
+                  type={showPass? "text": "password"}
+                  id="password" 
+                  className=" w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" 
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)} 
+                  placeholder="Password"
+                    
+                  />
+                  {showPass? 
+                    <AiFillEyeInvisible className=" absolute right-3 top-3 text-xl cursor-pointer" onClick={() => setShowPass(!showPass)}/> : 
+                    <AiFillEye className=" absolute right-3 top-3 text-xl cursor-pointer" onClick={() => setShowPass(!showPass)}/>
+                  }
+            </div>
+            <div className="justify-between flex whitespace-nowrap text-sm sm:text-lg">
+              <p className=" mb-6">Don't have an account? <Link to="/sign-up" className=" text-red-600 hover:text-red-700 transition duration-200 ease-in-out">Register</Link></p>
+              <p><Link to="/forgot-password" className=" text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out">Forgot Password</Link></p>
+            </div>
+            <button type="submit" className=" w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-800 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">Sign In</button>
+          </form>
+          <div className=" my-4 flex items-center  before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+            <p className=" text-center font-semibold mx-4">OR</p>
+          </div>
+          <OAuth/>
+        </div>
+      </div>
+    </section>
   )
 }
 
