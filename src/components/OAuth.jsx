ab370771a346bb47ff8a3a6ca4e24e7510759486
Hatif <md.hatifosmani15@gmail.com>
2023-09-09 04:58:16 +0530

    Google signup

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,37 @@
 import {FcGoogle} from "react-icons/fc";
+import { toast } from "react-toastify";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const OAuth = () => {
+  const navigate = useNavigate();
+  const onSubmit = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      // checking for the useer;
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()){
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Could not authenticate with Google.")
+    }
+  };
   return (
-    <button className=" flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+    <button 
+      className=" flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+      onClick={onSubmit}>
         <FcGoogle className=" text-2xl bg-white rounded-full mr-2"/>
         Continue with Google
     </button>
